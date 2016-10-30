109abc0224ae4cea46536521e131e628a43e241d
takujifunao <takuji.funao@gmail.com>
2016-10-31 04:26:43 +0900

    create mailcontent & set subject

--- classes/send_mail.rb
+++ classes/send_mail.rb
@@ -7,32 +7,25 @@ ActionMailer::Base.smtp_settings = {
 	:address => 'localhost'
 }
 
-
 class SendMail < ActionMailer::Base
 
 	default from: 'notice_maimaiblog@nogizaka.co'
 
 	def confirm(address, confirm_id)
 		@url = "http://maimaiblog.nogizaka.co/confirm?id=" + confirm_id
-		mail to: address
-		mail subject: "深川麻衣ブログ通知 確認メール"
-		mail do |format|
+		mail(to: address, subject: "深川麻衣ブログ通知 確認メール") do |format|
 			format.text
 		end
 	end
 
 	def confirm_ok(address)
-		mail to: address
-		mail subject: "深川麻衣ブログ通知 テストメールです"
-		mail do |format|
+		mail(to: address, subject: "深川麻衣ブログ通知 テストメールです") do |format|
 			format.text
 		end
 	end
 
 	def notice(address)
-		mail to: address
-		mail subject: "ブログ更新したよー！"
-		mail do |format|
+		mail(to: address, subject: "【深川麻衣】ブログ更新しました！") do |format|
 			format.text
 		end
 	end
