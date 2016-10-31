a45974b105dc60ae8301b6a6fdd6d6d9a67f53b1
takujifunao <takuji.funao@gmail.com>
2016-10-31 16:33:05 +0900

    test active-record

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,8 @@
-require 'yaml'
+require 'active_record'
 require './classes/update_checker.rb'
+
+ActiveRecord::Base.configurations = YAML::load(ERB.new(File.read('./config/database.yml')).result)
+ActiveRecord::Base.establish_connection(:development)
 
 every 1.minute do
 	UpdateChecker.check("http://tencarat.com/talent/talent042/diary/")
